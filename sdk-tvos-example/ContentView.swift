ea86b8fc66fccd609e9e412d6d4dfb2feb3000f8
Ladislav Navratil <navratill@gmail.com>
2022-03-02 02:46:48 +0100

    Initial Commit

---
+++ sdk-tvos-example/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sdk-tvos-example
+//
+//  Created by Ladislav Navratil on 02.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sdk-tvos-example/sdk_tvos_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sdk_tvos_exampleApp.swift
+//  sdk-tvos-example
+//
+//  Created by Ladislav Navratil on 02.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct sdk_tvos_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
