9932d70cea357eca35fcc81820cf00f24b2b219f
Jan (Honza) Jiráň <jan.jiran@tivio.studio>
2024-06-25 23:48:06 +0200

    feat,fix(tivio): fix timeshift playback and add tivio.isInitialized notification

--- sdk-tvos-example/sdk_tvos_exampleApp.swift
+++ sdk-tvos-example/sdk_tvos_exampleApp.swift
@@ -1,29 +1,32 @@
-//
-//  sdk_tvos_exampleApp.swift
-//  sdk-tvos-example
-//
-//  Created by Ladislav Navratil on 02.03.2022.
-//
-
 import SwiftUI
 import Tivio
 
 @main
 struct example_tvosApp: App {
-  
-  var tivio: Tivio
-  var playerViewModel = PlayerViewModel()
-  var programViewModel = ProgramViewModel()
-  
-  init() {
-    tivio = Tivio(secret: "QzY8vor8x0G6rooCWqzI", deviceCapabilities: [], verbose: true)
-  }
-  
-  var body: some Scene {
-    WindowGroup {
-      ContentView()
-        .environmentObject(playerViewModel)
-        .environmentObject(programViewModel)
+    var tivio: Tivio
+    var playerViewModel = PlayerViewModel()
+    var programViewModel = ProgramViewModel()
+
+    init() {
+        tivio = Tivio(secret: "QzY8vor8x0G6rooCWqzI", deviceCapabilities: [], verbose: true)
+
+        NotificationCenter.default.addObserver(
+            forName: NSNotification.Name("TivioInitializationChanged"),
+            object: nil,
+            queue: .main
+        ) { notification in
+          if let isInitialized = notification.userInfo?["isInitialized"] as? Bool {
+                print("Tivio is initialized: \(isInitialized)")
+                // Perform any additional actions when isInitialized changes
+            }
+        }
     }
-  }
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(playerViewModel)
+                .environmentObject(programViewModel)
+        }
+    }
 }
